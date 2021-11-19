cd3a357d09e905fc67b3c28f70580de8c2f36b8f
he <bgyhenrietta@yahoo.com>
2021-11-19 21:35:25 +0100

    week06 day04 layeredBusinessModel

---
+++ week06-lab-tasks/src/main/java/day04/layered/HangmanMain.java
@@ -0,0 +1,44 @@
+package day04.layered;
+
+import java.util.Scanner;
+
+public class HangmanMain {
+
+    public static void main(String[] args) {
+        new HangmanMain().run();
+    }
+
+    private void run() {
+        BusinessModel businessModel = new BusinessModel("szarvasmarha", 8);
+        Scanner scanner = new Scanner(System.in);
+        do {
+            printStatus(businessModel);
+            String guess = inputGuess(scanner);
+            boolean success = businessModel.takeGuess(guess);
+            if (success) {
+                System.out.println("Jó!");
+            } else {
+                System.out.println("Rossz!");
+            }
+        } while (!businessModel.won() && businessModel.hasMoreChance());
+        printResult(businessModel);
+    }
+
+    private void printStatus(BusinessModel businessModel) {
+        System.out.println("Itt tartasz: " + businessModel.getWordFound());
+        System.out.println("Ennyi tipped van: " + businessModel.getChances());
+    }
+
+    private String inputGuess(Scanner scanner) {
+        System.out.println("Mi a tipped?");
+        return scanner.nextLine();
+    }
+
+    private void printResult(BusinessModel businessModel) {
+        if (businessModel.won()) {
+            System.out.println("Ügyes!");
+        } else {
+            System.out.println("Vesztettél!");
+        }
+    }
+}
